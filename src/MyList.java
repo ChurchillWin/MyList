a64106a906d216ca632b42734527388cca250713
ChurchillWin <68127649+ChurchillWin@users.noreply.github.com>
2021-07-07 11:15:49 +0200

    Basic functionality of a generic List

--- src/MyList.java
+++ src/MyList.java
@@ -4,8 +4,115 @@ import java.util.*;
 
 public class MyList<T> {
     private int size;
-    private ListElement<T> start;
-    private ListElement<T> end;
+    private ListElement<T> start = new ListElement<>(null);
+    private ListElement<T> end = new ListElement<>(null);
 
+    MyList() {
+        size = 0;
+        this.start.setPrev(null);
+        this.end.setNext(null);
+        start.setNext(start);
+        end.setPrev(start);
+    }
+
+    public void insertEnd(T value) {
+        ListElement<T> element = new ListElement<>(value);//element to insert
+        ListElement<T> mem = new ListElement<>(null);//memory
+        end.getPrev().setNext(mem);
+        mem.setPrev(end.getPrev());// remember previous element
+        /*new element next => end
+        * end prev => new element */
+        element.setNext(end);
+        end.setPrev(element);
+        /*previous element next => new element
+        * new element prev => previous element */
+        mem.getPrev().setNext(element);
+        element.setPrev(mem.getPrev());
+        size++;
+    }
+    public void insertStart(T value) {
+        ListElement<T> element = new ListElement<>(value);//element to insert
+        ListElement<T> mem = new ListElement<>(null);//memory
+        start.getNext().setPrev(mem);
+        mem.setNext(start.getNext());
+        /*new element prev => start
+         * start next => new element */
+        element.setPrev(start);
+        start.setNext(element);
+        /*previous first element prev => new element
+         * new element next => previous first element */
+        mem.getNext().setPrev(element);
+        element.setNext(mem.getNext());
+        size++;
+    }
+    public T getLast() {
+        return end.getPrev().getValue();
+    }
+    public T getFirst() {
+        return start.getNext().getValue();
+    }
+    public void clear() {
+        start.setNext(end);
+        end.setPrev(start);
+    }
+    public T get(int elementIndex) {
+        if (elementIndex > size) {throw new IndexOutOfBoundsException("Index: " + elementIndex + " is not in list.size: " + size);}
+        ListElement<T> temp;
+        if (elementIndex <= size / 2) {
+            temp = start;
+            for (int j = 0; j <= size; j++) {
+                temp = temp.getNext();
+                if(j == elementIndex) {
+                    return temp.getValue();
+                }
+            }
+        }
+        else {
+            temp = end;
+            for (int j = size - 1; j >= 0; j--) {
+                temp = temp.getPrev();
+                if(j == elementIndex) {
+                    return temp.getValue();
+                }
+            }
+        }
+        throw new IndexOutOfBoundsException("Index: " + elementIndex + " is not in list.size: " + size);
+    }
+
+    private class ListElement<T> {
+        private T value;
+        private ListElement<T> next;
+        private ListElement<T> prev;
+
+        protected ListElement(T value) {
+            this.value = value;
+            next = null;
+            prev = null;
+        }
+        protected ListElement() {
+            this.value = null;
+            next = null;
+            prev = null;
+        }
+
+        public ListElement<T> getNext() {
+            return next;
+        }
+
+        public void setNext(ListElement<T> next) {
+            this.next = next;
+        }
+
+        public ListElement<T> getPrev() {
+            return prev;
+        }
+
+        public void setPrev(ListElement<T> prev) {
+            this.prev = prev;
+        }
+        public T getValue() {
+            return value;
+        }
+    }
 
 }
--- src/MyListTest.java
+++ src/MyListTest.java
@@ -5,8 +5,41 @@
     import org.junit.jupiter.api.Test;
 
 public class MyListTest {
+    static MyList<Integer> list;
+    private Object IndexOutOfBoundsException;
+
     @BeforeAll
     static void initAll() {
-       MyList<Integer> list = new MyList<>();
+        list = new MyList<>();
+
+        list.insertEnd(10);//3
+        list.insertEnd(12);//4
+        list.insertEnd(32);//5
+        list.insertStart(15);//2
+        list.insertStart(555);//1
+        list.insertStart(666);//0
+        list.insertEnd(5);//6
     }
+
+    @Test
+    void returnFirstElementValue() {
+        assertEquals(666, list.getFirst());
+    }
+
+    @Test
+    void returnLastElementValue() {
+        assertEquals(5, list.getLast());
+    }
+
+    @Test
+    void returns2ndElementValue() {
+        assertEquals(555, list.get(1));
+    }
+    @Test
+    void return6thElementValue() {
+        //with backwards iteration
+        assertEquals(32, list.get(5));
+    }
+
+
 }
